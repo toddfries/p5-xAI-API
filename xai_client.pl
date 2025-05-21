25eb4fe68d691f5ce2e2c4bfb2220fa3c1fc90a7
Todd T. Fries <todd@fries.net>
2025-05-21 16:06:09 -0500

    read config, add -c conf, -s section, -m model, -a action; flesh out -a search

--- xai_client.pl
+++ xai_client.pl
@@ -17,25 +17,60 @@
 use strict;
 use warnings;
 use xAI::API;
+use ReadConf;
+use Getopt::Std;
+use Data::Dumper;
 
-my $token = $ENV{XAI_BEARER_TOKEN};
+our $opt_c = $ENV{HOME}."/.config/cxai/grok.conf";
+our $opt_s = "creds"; # section
+our $opt_m = "grok-3-mini"; # model
+our $opt_a = "search"; # action
+
+getopts("c:m:s:");
+
+my $rc = ReadConf->new();
+my $conf = $rc->readconf($opt_c);
+my $section = $opt_s;
+my $c = $conf->{$section};
+if (!defined($c)) {
+        die "[creds] not found in ".$opt_c;
+}
+my $token = $c->{bearer};
 if (!defined($token)) {
-	die "Token not in XAI_BEARER_TOKEN environment variable";
+	die "Token not in config file!";
+}
+
+if (!defined($token)) {
+	die "Token not in ${opt_c} section [${opt_s}] var bearer";
 }
 
 # Assuming you've set your token in an environment variable for security
-my $api = xAI::API->new(bearer_token => $ENV{XAI_BEARER_TOKEN});
+my $api = xAI::API->new(bearer_token => $token, model => $opt_m);
 
 # Example query
 my $res;
 
-#$res = $api->query_grok("What is the current weather in San Francisco?");
-if (0) {
+if ($opt_a eq "models") {
+	$res = $api->models;
+}
+if ($opt_a eq "query") {
 	$res = $api->query_grok("Testing. Just say hi and hello world and nothing else.");
 
 	# Print the response
-	print "Grok says: ".$res->{response}."\n";
+	print "Grok says: ".${ $res->{choices} }[0]->{message}->{content}."\n";
 }
+if ($opt_a eq "search") {
+	$api->searchmode("on");
+	$api->citations("true");
+	$api->sources("x:x_handles=RapidResponse47,potus,whitehouse,presssec,lauraloomer,vigilantfox;web:excluded_websites=wikipedia.org,wokepedia.org:country=US;news:excluded_websites=abc.com,nbc.com,msnbc.com,cnn.com");
+	$res = $api->query_grok("What has President Trump done in the last week?");
+	my $i=0;
+	for my $choice (@{ $res->{choices}}) {
+		$i++;
+		printf "%3d. '%s'\n", $i, $choice->{message}->{content};
+		print Dumper($choice);
+	}
+}	
 
 if (0) {
 	$res = $api->keyinfo;
@@ -43,7 +78,7 @@ if (0) {
 if (0) {
 	$res = $api->list_embedding_models;
 }
-if (1) {
+if (0) {
 	$res = $api->language_models;
 }
 use Data::Dumper;
